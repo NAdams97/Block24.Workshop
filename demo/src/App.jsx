56df0d9c717752406539638e7d6059a272daef3d
NAdams97 <nitoad97@gmail.com>
2023-11-03 14:37:48 -0400

    second tab done

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
+import { puppyList } from './data'
+import { useState } from 'react'
 
 function App() {
-  
-
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log(puppies)
   return (
     <>
-    
+    {
+      puppies.map((puppy) => {
+        return <p> {puppy.name}</p>
+      })
+    }
     </>
   )
 }
